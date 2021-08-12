50df29f804ef3b1c10b4bc23fbde35f6172e87d9
George <kositskiy2003@mail.ru>
2021-08-12 20:49:27 +0300

    added some functions

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,32 @@
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close"),
+  closeElem = menu.querySelector(".menu__close"),
+  menuOverlay = menu.querySelector(".menu__overlay"),
   percent = document.querySelectorAll(".statistic__percent"),
   lines = document.querySelectorAll(".statistic__line span");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
-  document.body.style.overflow = "hidden";
-});
+hamburger.addEventListener("click", showHamburger);
 
-closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
-  document.body.style.overflow = "";
-});
+closeElem.addEventListener("click", hideHamburger);
 
 percent.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
+});
+
+function showHamburger() {
+  menu.classList.add("active");
+  document.body.style.overflow = "hidden";
+}
+
+function hideHamburger() {
+  menu.classList.remove("active");
+  document.body.style.overflow = "";
+}
+
+menuOverlay.addEventListener("click", hideHamburger);
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape" || menu.classList.contains("active")) {
+    hideHamburger();
+  }
 });
