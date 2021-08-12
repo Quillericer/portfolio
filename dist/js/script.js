734d5a48e507c7b7566af4abb9e1b4bc4786e3bc
George <kositskiy2003@mail.ru>
2021-08-12 14:24:03 +0300

    changed everything to my personality

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,19 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close'),
-    percent = document.querySelectorAll('.statistic__percent'),
-    lines = document.querySelectorAll('.statistic__line span');
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu__close"),
+  percent = document.querySelectorAll(".statistic__percent"),
+  lines = document.querySelectorAll(".statistic__line span");
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-    document.body.style.overflow = 'hidden';
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
+  document.body.style.overflow = "hidden";
 });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-    document.body.style.overflow = '';
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
+  document.body.style.overflow = "";
 });
 
 percent.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
+  lines[i].style.width = item.innerHTML;
 });
